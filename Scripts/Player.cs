0563bc33188e677007b14a0ad7a9a362b307d0f7
AbelLagonell <alagonell1730@floridapoly.edu>
2025-02-10 16:33:07 -0500

    Started CS work

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -3,18 +3,11 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	
-	
 	[Export] public  float Speed = 300.0f;
-	[Export] public Resource MoveAction = null;
-
-	public override void _Ready()
-	{
-		this.Set("property", "GUIDEAction");
-		GD.Print(this.Get("property"));
-	}
+	[Export] public Resource MoveAction = null; // TYPE GUIDEAction
 
 	public override void _PhysicsProcess(double delta)
 	{
+		Position += (Vector2)(MoveAction.Get("value_axis_2D")) * Speed * (float)delta;
 	}
 }
