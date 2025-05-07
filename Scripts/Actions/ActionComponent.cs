987bdd09abca700c13552eb5960f14f1b115f918
AbelLagonell <alagonell@outlook.com>
2025-05-07 12:59:52 -0400

    Updating it to Godot 4

--- Scripts/Actions/ActionComponent.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using Godot;
-using Godot.Collections;
-
-namespace silver.Scripts.Actions;
-
-[GlobalClass, Tool]
-public abstract partial class ActionComponent : Node2D { }
--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,31 +1,34 @@
-using System;
 using Godot;
-using silver.Scripts.Actions;
+using System;
 
-namespace silver.Scripts;
+public partial class Player : CharacterBody2D
+{
+    public const float Speed = 300.0f;
+    public const float JumpVelocity = -400.0f;
 
-internal struct AvailableActions {
-    const string Idle = "Idle";
-    const string Walk = "Walk";
-
-    //const string Jab = "Jab";
-    //const string Cross = "Cross";
-}
-
-public partial class Player : CharacterBody2D {
-    [Export] private ActionManager _actionManager = null;
-
-    public override void _Ready() {
-        _actionManager = GetNode<ActionManager>("ActionManager");
-    }
-
-    public override void _PhysicsProcess(double delta) {
+    public override void _PhysicsProcess(double delta)
+    {
         Vector2 velocity = Velocity;
 
-        Vector2 direction = Input.GetVector("mv_left", "mv_right", "mv_up", "mv_down");
+        // Add the gravity.
+        if (!IsOnFloor()) {
+            velocity += GetGravity() * (float)delta;
+        }
 
-        
-        
+        // Handle Jump.
+        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor()) {
+            velocity.Y = JumpVelocity;
+        }
+
+        // Get the input direction and handle the movement/deceleration.
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero) {
+            velocity.X = direction.X * Speed;
+        } else {
+            velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+        }
+
         Velocity = velocity;
         MoveAndSlide();
     }
